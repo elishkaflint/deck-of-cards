5256db6853aa316f654e092648422242748c74a1
Elishka Flint <elishkaf@gmail.com>
2018-09-15 10:46:29 +0100

    TEST Add unit test for deck of card initialization

---
+++ spec/deck-spec.js
@@ -0,0 +1,8 @@
+describe("UNIT TESTS: Deck of Cards", function() {
+
+  it('Initializes with a brand new pack of cards', function() {
+    let deck = new Deck();
+    expect(deck.getCards()).toEqual(NEW_PACK_OF_CARDS);
+  });
+
+});
