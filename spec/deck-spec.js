0c5cde806865e257d4066b70596b99aabc5f5498
Elishka Flint <elishkaf@gmail.com>
2018-09-15 13:17:09 +0100

    TEST Add custom matcher to spec to check that cards are not in sequence once shuffled

--- spec/deck-spec.js
+++ spec/deck-spec.js
@@ -1,5 +1,21 @@
 describe("UNIT TESTS: Deck", function() {
 
+  beforeEach(function() {
+    jasmine.addMatchers({
+      toHaveNoCardsInSequence: function(){
+        return {
+          compare: function(object){
+            var result = { pass: hasNoSequentialElements(PERFECT_SEQUENCE, object) };
+            if(!result.pass){
+              result.message = 'Expected ' + object + ' to have no sequential elements';
+            }
+            return result;
+          }
+        }
+      }
+    });
+  });
+
   describe('#new', function() {
     it('Initializes with a brand new deck of cards', function() {
       let deck = new Deck();
