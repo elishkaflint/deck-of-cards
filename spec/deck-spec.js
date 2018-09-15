64a84ffb34ab905f95e6a454a6f80008ef9044a8
Elishka Flint <elishkaf@gmail.com>
2018-09-15 12:17:27 +0100

    TEST Add unit test for shuffle method in deck class

--- spec/deck-spec.js
+++ spec/deck-spec.js
@@ -1,8 +1,18 @@
 describe("UNIT TESTS: Deck", function() {
 
-  it('Initializes with a brand new deck of cards', function() {
-    let deck = new Deck();
-    expect(deck.getCards()).toEqual(PERFECT_SEQUENCE);
+  describe('#new', function() {
+    it('Initializes with a brand new deck of cards', function() {
+      let deck = new Deck();
+      expect(deck.getCards()).toEqual(PERFECT_SEQUENCE);
+    });
+  });
+
+  describe('#shuffle', function() {
+    it('Can be shuffled out of sequence', function() {
+      let deck = new Deck();
+      deck.shuffle();
+      expect(deck.getCards()).toHaveNoCardsInSequence();
+    });
   });
 
 });
