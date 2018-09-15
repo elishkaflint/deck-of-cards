e4bf92095639233a21a03b22d502f870e96e2db5
Elishka Flint <elishkaf@gmail.com>
2018-09-15 10:25:53 +0100

    OTHER Set up testing environment

---
+++ spec/test-spec.js
@@ -0,0 +1,9 @@
+describe("UNIT TESTS: Item", function() {
+
+  describe(".testingWorking", function() {
+    it('shows that the test environment is working', function() {
+      expect(testingWorking()).toEqual('testing working');
+    });
+  });
+
+});
---
+++ src/test.js
@@ -0,0 +1,3 @@
+function testingWorking() {
+  return 'testing working';
+}
