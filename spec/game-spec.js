883e02699117ce9e784f98f9877d81d2d8f56a4a
Elishka Flint <elishkaf@gmail.com>
2018-09-17 08:00:38 +0100

    TEST Add unit test for shuffle method in game class

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,19 +1,32 @@
 describe("UNIT TESTS: Game", function() {
 
   describe('#new', function() {
-
     it('Initializes with a brand new deck of cards', function() {
-
       class dummyDeck {
         constructor() {
           this.cards = 'cards'
         }
       }
-
       let game = new Game(dummyDeck);
       expect(game.getCards()).toEqual('cards');
     });
-
   });
+
+  describe('#shuffle', function() {
+    it('Shuffles the deck of cards', function() {
+      class dummyDeck {
+        constructor() {
+          this.cards = 'cards'
+        }
+        shuffle() {
+          return 'cards shuffled'
+        }
+      }
+      let game = new Game(dummyDeck);
+      console.log(game.cards)
+      game.deck.shuffle();
+      expect(game.getCards()).toEqual('cards shuffled')
+    })
+  })
 
 });
