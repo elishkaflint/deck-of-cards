22f8142cbb6315b30bd5016947be12263850b37c
Elishka Flint <elishkaf@gmail.com>
2018-09-17 18:33:25 +0100

    OTHER Fix merge conflicts after restoring stash

--- src/deck.js
+++ src/deck.js
@@ -10,13 +10,38 @@ class Deck {
   }
 
   shuffle() {
-    const initialDeck = this.cards.slice(0);
-    const shuffledDeck = [];
-    while (initialDeck.length > 0) {
-      const position = Math.floor(Math.random() * initialDeck.length);
-      shuffledDeck.push(initialDeck[position]);
-      initialDeck.splice(position, 1);
-    }
-    this.cards = shuffledDeck;
+    let initialDeck = this.cards.slice(0);
+    let shuffledDeck = []
+
+    let position = Math.floor(Math.random() * initialDeck.length)
+    shuffledDeck.push(initialDeck[position])
+    initialDeck.splice(position, 1)
+
+    console.log(initialDeck)
+    console.log(shuffledDeck)
+
+    // while (shuffledDeck.length <= PERFECT_SEQUENCE.length) {
+    //   let position = Math.floor(Math.random() * initialDeck.length)
+    //   let randomCard = initialDeck[position]
+    //   let index = PERFECT_SEQUENCE.indexOf(randomCard)
+    //   // will this selection put it next to a consecutive card?
+    //   if(shuffledDeck[shuffledDeck.length - 1] !== PERFECT_SEQUENCE[index - 1]){
+    //     shuffledDeck.push(randomCard);
+    //     initialDeck.splice(position,1);
+    //   }
+    // }
+
   }
+
+
+
+      // let initialDeck = this.cards.slice(0);
+      // let shuffledDeck = []
+      //
+      //   shuffledDeck.push(initialDeck[position])
+      //   initialDeck.splice(position,1)
+      // }
+      // this.cards = shuffledDeck;
+
+
 }
