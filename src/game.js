2fdad7be61dc61ab3d548be72d8f3721113c4b2f
Elishka Flint <elishkaf@gmail.com>
2018-09-17 14:08:04 +0100

    MODEL Implement deal function to pass unit test

--- src/game.js
+++ src/game.js
@@ -13,7 +13,18 @@ class Game {
   }
 
   deal() {
-    
+    this.createPlayers();
+    for (var i = 0; i < DEFAULT_NUMBER_OF_CARDS; i++) {
+      for (var j = 0; j < DEFAULT_NUMBER_OF_PLAYERS; j++) {
+        this.hands[j][i] = this.getCards().shift()
+      }
+    }
+  }
+
+  createPlayers() {
+    for (var i = 1; i <= DEFAULT_NUMBER_OF_PLAYERS; i++) {
+      this.hands.push([]);
+    }
   }
 
 }
