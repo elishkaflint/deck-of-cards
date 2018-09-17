b2ccd5a692cfa107967eb84206d8e2282b29c7fe
Elishka Flint <elishkaf@gmail.com>
2018-09-17 11:25:57 +0100

    TEST Add feature test for third user story

--- spec/feature-spec.js
+++ spec/feature-spec.js
@@ -16,21 +16,31 @@ describe("FEATURE TESTS: Game of Cards", function() {
     });
   });
 
-    // As a dealer,
-    // So that I can set up a game of cards,
-    // I want to start with a deck of 52 cards in perfect sequence
-    it('The game starts with a brand new deck of cards', function() {
-      let game = new Game();
-      expect(game.getCards()).toEqual(PERFECT_SEQUENCE);
-    });
+  // As a dealer,
+  // So that I can set up a game of cards,
+  // I want to start with a deck of 52 cards in perfect sequence
+  it('The game starts with a brand new deck of cards', function() {
+    let game = new Game();
+    expect(game.getCards()).toEqual(PERFECT_SEQUENCE);
+  });
 
-    // As a dealer,
-    // So that I can deal a random hand to each player,
-    // I want to be able to shuffle the cards so no two cards are still in sequence
-    it('The pack of cards can be shuffled so that no cards remain in sequence', function() {
-      let game = new Game();
-      game.shuffle();
-      expect(game.getCards()).toHaveNoCardsInSequence();
-    });
+  // As a dealer,
+  // So that I can deal a random hand to each player,
+  // I want to be able to shuffle the cards so no two cards are still in sequence
+  it('The pack of cards can be shuffled so that no cards remain in sequence', function() {
+    let game = new Game();
+    game.shuffle();
+    expect(game.getCards()).toHaveNoCardsInSequence();
+  });
+
+  // As a player,
+  // So that I can start the game,
+  // I want to be able to deal out 7 cards to 4 players
+  it('A hand of cards can be dealt to a given number of players', function() {
+    let game = new Game();
+    game.shuffle();
+    game.deal();
+    expect(game.players).toHaveCorrectHand();
+  });
 
 });
